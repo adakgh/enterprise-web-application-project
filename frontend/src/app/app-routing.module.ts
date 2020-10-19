c258d35074adbc68c8a7b21b66b90dcf4b4516db
Omer Citik <omer.citik@hva.nl>
2020-10-19 02:06:46 +0200

    Squashed commit of the following: Try Merge SupplierPage branch to master
    commit e4c9e55b5291a9dfd314a41388cf0f42c8854928
    Author: Omer Citik <omer.citik@hva.nl>
    Date:   Fri Oct 16 20:43:15 2020 +0200
    
        SupplierInfoEdit component created and implemented
    
        The page for editing the supplier info like the name, email, adress
    
    commit 401f5aa0c9ed684cda668a43e478daf3d457d619
    Author: Omer Citik <omer.citik@hva.nl>
    Date:   Fri Oct 16 18:15:52 2020 +0200
    
        Child component Recent Product component added and implemented
    
    commit 205605643cdb4e3fad0bb665ff12b4ae7df8d612
    Author: Omer Citik <omer.citik@hva.nl>
    Date:   Fri Oct 16 15:15:22 2020 +0200
    
        Supplier Info page base done
    
    commit 123777b0461ea883547cd745a4d6e23481be36cb
    Author: Omer Citik <omer.citik@hva.nl>
    Date:   Fri Oct 16 12:17:05 2020 +0200
    
        Created Supplier Info Component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {UserComponent} from './components/user/user.component';
 import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/login/login.component';
 import {AuthGuardService} from './guards/auth-guard.service';
 import {LogoutComponent} from './components/logout/logout.component';
 import {ProductComponent} from './components/product/product.component';
+import {SupplierInfoComponent} from './components/supplier-info/supplier-info.component';
+import {SupplierInfoEditComponent} from './components/supplier-info/supplier-info-edit/supplier-info-edit.component';
 import {ProductDetailComponent} from './components/product-detail/product-detail.component';
 
 const routes: Routes = [
@@ -14,11 +16,15 @@ const routes: Routes = [
     {path: 'products', component: ProductComponent, canActivate: [AuthGuardService]},
     {path: 'product-detail', component: ProductDetailComponent, canActivate: [AuthGuardService]},
     {path: 'login', component: LoginComponent},
-    {path: 'logout', component: LogoutComponent}
+    {path: 'logout', component: LogoutComponent},
+    {path: 'supplierinfo', component: SupplierInfoComponent},
+    {path: 'supplierinfo/edit', component: SupplierInfoEditComponent},
+    {path: '**', redirectTo: ''} // Would be nicer to have a 404 ERROR component
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,36 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { UserComponent } from './components/user/user.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import {FormsModule} from '@angular/forms';
+import { LogoutComponent } from './components/logout/logout.component';
+import { ProductComponent } from './components/product/product.component';
+import { SupplierInfoComponent } from './components/supplier-info/supplier-info.component';
+import { RecentProductComponent } from './components/supplier-info/recent-product/recent-product.component';
+import { SupplierInfoEditComponent } from './components/supplier-info/supplier-info-edit/supplier-info-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    HomeComponent,
+    LoginComponent,
+    LogoutComponent,
+    ProductComponent,
+    SupplierInfoComponent,
+    RecentProductComponent,
+    SupplierInfoEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 import {UserComponent} from './components/user/user.component';
 import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/login/login.component';
