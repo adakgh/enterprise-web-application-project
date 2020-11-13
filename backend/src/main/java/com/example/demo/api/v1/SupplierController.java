d3408cfc515139d6bd252a4b793fd77f1fc20e41
indemar001 <roberto.indemans@hva.nl>
2020-11-13 12:29:38 +0100

    new endpoint added to update supplier data

--- backend/src/main/java/com/example/demo/api/v1/SupplierController.java
+++ backend/src/main/java/com/example/demo/api/v1/SupplierController.java
@@ -1,8 +1,10 @@
 package com.example.demo.api.v1;
 
+import com.example.demo.models.RoleType;
 import com.example.demo.persistence.entities.SupplierEntity;
 import com.example.demo.services.SupplierService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,13 +14,20 @@ public class SupplierController {
 
     private final SupplierService supplierService;
 
+    /**
+     * Retrieves supplier-info with the given supplier-id.
+     */
     @GetMapping("/{id}")
     public SupplierEntity getSupplierInfo(@PathVariable long id) {
         return supplierService.findById(id);
     }
 
-    @PutMapping("/{id}")
-    public SupplierEntity updateSupplierInfo(@PathVariable long id) {
-        return supplierService.findById(id);
+    /**
+     * Updates the supplier-info of the current logged-in user.
+     */
+    @Secured(RoleType.SUPPLIER)
+    @PutMapping
+    public void updateSupplierInfo(@RequestBody SupplierEntity supplier) {
+        supplierService.updateById(supplier);
     }
 }
