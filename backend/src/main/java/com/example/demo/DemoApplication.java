a27393a83bd19d8e17cb4f9c1235be6d79f48a9f
indemar001 <roberto.indemans@hva.nl>
2020-11-16 12:52:15 +0100

    added more suppliers and products, and finishing touches to the AddProductComponent

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -61,6 +61,10 @@ public class DemoApplication {
             pCat6.setName("Vega");
             productCategoryRepository.save(pCat6);
 
+            var pCat7 = new ProductCategoryEntity();
+            pCat7.setName("Overig");
+            productCategoryRepository.save(pCat7);
+
             // -----------------------------------------------------
 
             // inquiry
@@ -95,7 +99,7 @@ public class DemoApplication {
 
             // -----------------------------------------------------
 
-            // supplier
+            // supplier 1
             var user1 = new UserEntity();
             user1.setUsername("myUsername@gmail.com");
             user1.setPassword(passwordEncoder.encode("myPassword1!"));
@@ -103,18 +107,18 @@ public class DemoApplication {
             user1.addRole(customerRole);
             user1.addRole(adminRole);
 
-            var supp = new SupplierEntity();
-            supp.setCompanyName("Slachterij Hooijmans");
-            supp.setContactPerson("Willem Hooijmans");
+            var supp1 = new SupplierEntity();
+            supp1.setCompanyName("Slachterij Hooijmans");
+            supp1.setContactPerson("Willem Hooijmans");
 
             // supplier address
-            var supp_addr = new SupplierAddressEntity();
-            supp_addr.setStreet("Scheerbiesstraat");
-            supp_addr.setNumber("3-A");
-            supp_addr.setPostalCode("4845 PL");
-            supp_addr.setCity("Wagenberg");
-            supp_addr.setCountry("Nederland");
-            supp.addAddress(supp_addr);
+            var supp1_addr = new SupplierAddressEntity();
+            supp1_addr.setStreet("Scheerbiesstraat");
+            supp1_addr.setNumber("3-A");
+            supp1_addr.setPostalCode("4845 PL");
+            supp1_addr.setCity("Wagenberg");
+            supp1_addr.setCountry("Nederland");
+            supp1.addAddress(supp1_addr);
 
             // supplier product
             var product1 = new ProductEntity();
@@ -123,8 +127,8 @@ public class DemoApplication {
             product1.setQuantity("per stuk:");
             product1.setPrice(new BigDecimal("4.95"));
             product1.setAddedDate(new Date());
-            product1.addProductCategory(productCategoryRepository.getOne(5L)); // lazy load
-            supp.addProduct(product1); // resolve relationship (supplier <-> product)
+            product1.addProductCategory(productCategoryRepository.getOne(1L)); // lazy load
+            supp1.addProduct(product1); // resolve relationship (supplier <-> product)
 
             var product2 = new ProductEntity();
             product2.setName("Kip filet");
@@ -132,16 +136,131 @@ public class DemoApplication {
             product2.setQuantity("per stuk:");
             product2.setPrice(new BigDecimal("1.85"));
             product2.setAddedDate(new Date());
-            product2.addProductCategory(productCategoryRepository.getOne(2L)); // lazy load
-            supp.addProduct(product2); // resolve relationship (supplier <-> product)
+            product2.addProductCategory(productCategoryRepository.getOne(5L)); // lazy load
+            supp1.addProduct(product2); // resolve relationship (supplier <-> product)
 
             // save to database
-            user1.setSupplier(supp);
+            user1.setSupplier(supp1);
             userRepository.save(user1);
 
             // -----------------------------------------------------
 
-            // create your own supplier here...
+            // supplier 2
+            var user2 = new UserEntity();
+            user2.setUsername("myUsername2@gmail.com");
+            user2.setPassword(passwordEncoder.encode("myPassword1!"));
+            user2.addRole(supplierRole);
+
+            var supp2 = new SupplierEntity();
+            supp2.setCompanyName("Boerderij Theo Machielsen");
+            supp2.setContactPerson("Theo Machielsen");
+
+            // supplier address
+            var supp2_addr = new SupplierAddressEntity();
+            supp2_addr.setStreet("Van Haestrechtstraat");
+            supp2_addr.setNumber("9-C");
+            supp2_addr.setPostalCode("4845 PL");
+            supp2_addr.setCity("Kaatsheuvel");
+            supp2_addr.setCountry("Nederland");
+            supp2.addAddress(supp2_addr);
+
+            // supplier product
+            var product2_1 = new ProductEntity();
+            product2_1.setName("Honing");
+            product2_1.setDescription("100% Natuurzuivere Honing");
+            product2_1.setQuantity("per stuk:");
+            product2_1.setPrice(new BigDecimal("6.95"));
+            product2_1.setAddedDate(new Date());
+            product2_1.addProductCategory(productCategoryRepository.getOne(7L)); // lazy load
+            supp2.addProduct(product2_1); // resolve relationship (supplier <-> product)
+
+            var product2_2 = new ProductEntity();
+            product2_2.setName("Champignons");
+            product2_2.setDescription("Filetlapje gemarineerd verpakt");
+            product2_2.setQuantity("per 500 gram:");
+            product2_2.setPrice(new BigDecimal("3.49"));
+            product2_2.setAddedDate(new Date());
+            product2_2.addProductCategory(productCategoryRepository.getOne(1L)); // lazy load
+            supp2.addProduct(product2_2); // resolve relationship (supplier <-> product)
+
+            var product2_3 = new ProductEntity();
+            product2_3.setName("Aardbeien");
+            product2_3.setDescription("De Hollandse aarbei, fris en vol van smaak!");
+            product2_3.setQuantity("per doos:");
+            product2_3.setPrice(new BigDecimal("2.79"));
+            product2_3.setAddedDate(new Date());
+            product2_3.addProductCategory(productCategoryRepository.getOne(1L)); // lazy load
+            supp2.addProduct(product2_3); // resolve relationship (supplier <-> product)
+
+            var product2_4 = new ProductEntity();
+            product2_4.setName("Volkoren Brood");
+            product2_4.setDescription("Volkoren brood voor een lekkere, stevige boterham.");
+            product2_4.setQuantity("per stuk:");
+            product2_4.setPrice(new BigDecimal("1.25"));
+            product2_4.setAddedDate(new Date());
+            product2_4.addProductCategory(productCategoryRepository.getOne(3L)); // lazy load
+            supp2.addProduct(product2_4); // resolve relationship (supplier <-> product)
+
+            var product2_5 = new ProductEntity();
+            product2_5.setName("Veganistische Mayonaise");
+            product2_5.setDescription("Mayonaise waarbj de eierdooier is vervangen door plantaardige ingrediënten.");
+            product2_5.setQuantity("per stuk:");
+            product2_5.setPrice(new BigDecimal("1.84"));
+            product2_5.setAddedDate(new Date());
+            product2_5.addProductCategory(productCategoryRepository.getOne(6L)); // lazy load
+            supp2.addProduct(product2_5); // resolve relationship (supplier <-> product)
+
+            var product2_6 = new ProductEntity();
+            product2_6.setName("Santpoorts Tripel");
+            product2_6.setDescription("Waar alles mee begon! Dit is ons aller-eerste bier, alleen voor echte mannen!");
+            product2_6.setQuantity("per stuk:");
+            product2_6.setPrice(new BigDecimal("2.49"));
+            product2_6.setAddedDate(new Date());
+            product2_6.addProductCategory(productCategoryRepository.getOne(4L)); // lazy load
+            supp2.addProduct(product2_6); // resolve relationship (supplier <-> product)
+
+            // save to database
+            user2.setSupplier(supp2);
+            userRepository.save(user2);
+
+            // -----------------------------------------------------
+
+            // supplier 3
+            var user3 = new UserEntity();
+            user3.setUsername("myUsername3@gmail.com");
+            user3.setPassword(passwordEncoder.encode("myPassword1!"));
+            user3.addRole(supplierRole);
+            user3.addRole(customerRole);
+            user3.addRole(adminRole);
+
+            var supp3 = new SupplierEntity();
+            supp3.setCompanyName("VITAMINEHOEK DE KRAGGE");
+            supp3.setContactPerson("Sonja de Kragge");
+
+            // supplier address
+            var supp3_addr = new SupplierAddressEntity();
+            supp3_addr.setStreet("Scheerbiesstraat");
+            supp3_addr.setNumber("3-A");
+            supp3_addr.setPostalCode("4845 PL");
+            supp3_addr.setCity("Wagenberg");
+            supp3_addr.setCountry("Nederland");
+            supp3.addAddress(supp3_addr);
+
+            // supplier product
+            var product3_1 = new ProductEntity();
+            product3_1.setName("Geitenkaas");
+            product3_1.setDescription("Honingklaver Biologische geitenkaas in blokjes in zonnebloemolie.");
+            product3_1.setQuantity("per stuk:");
+            product3_1.setPrice(new BigDecimal("6.25"));
+            product3_1.setAddedDate(new Date());
+            product3_1.addProductCategory(productCategoryRepository.getOne(2L)); // lazy load
+            supp3.addProduct(product3_1); // resolve relationship (supplier <-> product)
+
+            // save to database
+            user3.setSupplier(supp3);
+            userRepository.save(user3);
+
+            // -----------------------------------------------------
         };
     }
 }
