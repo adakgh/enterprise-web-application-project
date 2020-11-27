98b46c66f60f4f5b2739c13bd77d3a2c18d955c9
kingilsildor <tychostam@gmail.com>
2020-11-27 17:10:11 +0100

    Update page for products

---
+++ frontend/src/app/components/myproducts/myproducts-edit/myproducts-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myproducts-edit',
+  templateUrl: './myproducts-edit.component.html',
+  styleUrls: ['./myproducts-edit.component.css']
+})
+export class MyproductsEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
