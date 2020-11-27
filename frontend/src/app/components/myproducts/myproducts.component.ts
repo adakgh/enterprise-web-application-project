ced44cdba301dbc14866014c5b0529ed9893367b
kingilsildor <tychostam@gmail.com>
2020-11-27 12:29:16 +0100

    Custom HTML and CSS for myproducts page

--- frontend/src/app/components/myproducts/myproducts.component.ts
+++ frontend/src/app/components/myproducts/myproducts.component.ts
@@ -3,6 +3,7 @@ import {ApiService} from '../../services/api.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {MyProductsService} from '../../services/my-products.service';
 import {RouteUtil} from '../../utils/route.util';
+import {ProductService} from '../../services/product.service';
 
 @Component({
     selector: 'app-myproducts',
@@ -10,14 +11,18 @@ import {RouteUtil} from '../../utils/route.util';
     styleUrls: ['./myproducts.component.css']
 })
 export class MyproductsComponent implements OnInit {
+
     jsonData: any[] = [];
+    categoryMap: any[] = [];
+    selectedCategories: any = [];
 
     constructor(
-        private apiService: ApiService,
         private router: Router,
-        private activatedRoute: ActivatedRoute,
+        private apiService: ApiService,
         private routeUtil: RouteUtil,
-        private myProductsService: MyProductsService
+        private productService: ProductService,
+        private myProductsService: MyProductsService,
+        private activatedRoute: ActivatedRoute
     ) {
     }
 
@@ -28,13 +33,9 @@ export class MyproductsComponent implements OnInit {
                 this.loadData();
             }
         );
-    }
-
-    loadData(): void {
-        this.myProductsService.getAllMyProducts().subscribe(
+        this.productService.getAllCategories().subscribe(
             res => {
-                this.jsonData = res;
-                console.log(res);
+                this.categoryMap = res;
             },
             err => {
                 console.log(err);
@@ -42,4 +43,37 @@ export class MyproductsComponent implements OnInit {
         );
     }
 
+    loadData(): void {
+        this.myProductsService.getAllMyProducts().subscribe(
+            res => {
+                this.jsonData = res;
+            },
+            err => {
+                console.log(err);
+            }
+        );
+    }
+
+    searchValue(values: any): void {
+        if (values.target.value !== '') {
+            this.routeUtil.addParam('name', '*' + values.target.value + '*');
+        } else {
+            this.routeUtil.deleteParam('name');
+        }
+    }
+
+    filterCategory(values: any): void {
+        if (values.target.checked) {
+            this.selectedCategories.push(values.target.value);
+        } else {
+            console.log('delete');
+            this.selectedCategories.splice(this.selectedCategories.indexOf(values.target.value), 1);
+        }
+        console.log(this.selectedCategories);
+        if (this.selectedCategories.length > 0) {
+            this.routeUtil.addParam('category', this.selectedCategories.join(','));
+        } else {
+            this.routeUtil.deleteParam('category');
+        }
+    }
 }
