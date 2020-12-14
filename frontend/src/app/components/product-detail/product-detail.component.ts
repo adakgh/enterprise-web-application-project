bd7f4dacee348680dd5b06531a9885fa1e274d94
eijkm4 <mikal.van.eijk@hva.nl>
2020-12-14 14:36:49 +0100

    Fixed the product-detail page, price and tpe were not showing because of the missing .content

--- frontend/src/app/components/product-detail/product-detail.component.ts
+++ frontend/src/app/components/product-detail/product-detail.component.ts
@@ -3,7 +3,7 @@ import {Router} from '@angular/router';
 import {ApiService} from '../../services/api.service';
 import {RouteUtil} from '../../utils/route.util';
 import {ProductService} from '../../services/product.service';
-import {DemoImage} from "../supplier-info/supplier-info-edit/default-image";
+import {DemoImage} from '../supplier-info/supplier-info-edit/default-image';
 
 @Component({
     selector: 'app-product-detail',
@@ -22,16 +22,15 @@ export class ProductDetailComponent implements OnInit {
         private routeUtil: RouteUtil,
         private productService: ProductService,
         public demoImage: DemoImage
-    ) {
-    }
+    ) {}
 
     ngOnInit(): void {
         this.productService.getAllProduct().subscribe(
             res => {
                 // console.log(res[0].customData.supplierId);
-                this.jsonData = res;
-                this.type = res[0].price.split(':')[0];
-                this.price = res[0].price.split(':')[1];
+                this.jsonData = res.content;
+                this.type = res.content[0].price.split(':')[0];
+                this.price = res.content[0].price.split(':')[1];
                 // console.log(this.price);
             },
             err => {
@@ -39,5 +38,4 @@ export class ProductDetailComponent implements OnInit {
             }
         );
     }
-
 }
